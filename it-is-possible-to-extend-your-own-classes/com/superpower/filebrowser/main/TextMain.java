415f457c25f485de9f0637be9483c888f52454c2
Rikard Fröberg <rikard@morus.se>
2022-04-28 22:50:30 +0200

    checkstyle

--- it-is-possible-to-extend-your-own-classes/com/superpower/filebrowser/main/TextMain.java
+++ it-is-possible-to-extend-your-own-classes/com/superpower/filebrowser/main/TextMain.java
@@ -1,23 +1,20 @@
 package com.superpower.filebrowser.main;
 
-import java.util.List;
-
-import com.superpower.filebrowser.utils.FileList;
-import com.superpower.filebrowser.FBFile;
 import com.superpower.filebrowser.FBDir;
+import com.superpower.filebrowser.FBFile;
 import com.superpower.filebrowser.FBJavaFile;
+import com.superpower.filebrowser.utils.FileList;
+import java.util.List;
 
 public class TextMain {
 
-    public static void main(String[] args) {
+  public static void main(String[] args) {
         
-        List<FBFile> files = FileList.list();
+    List<FBFile> files = FileList.list();
 
-        for (FBFile f : files) {
-            System.out.println(f.thumbnail() + " " + f.name());
-        }
-        
-    }
+    for (FBFile f : files) {
+      System.out.println(f.thumbnail() + " " + f.name());
+    }        
+  }
 
 }
-    
