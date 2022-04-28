6910fda79ed5706a62e696367cb439e2ca656494
Rikard Fröberg <rikard@morus.se>
2022-04-28 22:43:09 +0200

    checkstyle

--- it-is-possible-to-extend-your-own-classes/com/superpower/filebrowser/FBDir.java
+++ it-is-possible-to-extend-your-own-classes/com/superpower/filebrowser/FBDir.java
@@ -2,13 +2,12 @@ package com.superpower.filebrowser;
 
 public class FBDir extends FBFile {
 
-    public FBDir(String name) {
-        super(name);
-    }
-    
-
-    public String thumbnail() {
-        return "[dir ]";
-    }
+  public FBDir(String name) {
+    super(name);
+  }
+  
+  public String thumbnail() {
+    return "[dir ]";
+  }
     
 }
