4bcfd8cd782229da9bdc9985346c56d58f419635
Rikard Fröberg <rikard@morus.se>
2022-04-28 22:31:30 +0200

    checkstyle

--- live-code/customer-example/com/ebiz/Customer.java
+++ live-code/customer-example/com/ebiz/Customer.java
@@ -4,61 +4,61 @@ import java.util.Objects;
 
 public class Customer {
 
-    private String name;
-    private String email;
+  private String name;
+  private String email;
 
-    public Customer(String name, String email) {
-        this.name  = name;
-        this.email = email;
-    }
+  public Customer(String name, String email) {
+    this.name = name;
+    this.email = email;
+  }
 
 
-    public String email() {
-        return email;
-    }
+  public String email() {
+    return email;
+  }
     
-    public String name() {
-        return name;
-    }
+  public String name() {
+    return name;
+  }
 
-    // Please compiler, make sure that this method overrides some
-    // method in my superclass(es)
-    @Override
-    public String toString() {
-        // Bob <bob@email.com>
-        return this.name + " <" + this.email + ">";
-    }
+  // Please compiler, make sure that this method overrides some
+  // method in my superclass(es)
+  @Override
+  public String toString() {
+    // Bob <bob@email.com>
+    return this.name + " <" + this.email + ">";
+  }
     
-    @Override
-    public int hashCode() {
-        int hash = Objects.hash(name, email);
+  @Override
+  public int hashCode() {
+    int hash = Objects.hash(name, email);
 
-        // Or another example:
-        //   int hash = name.hashCode()+email.hashCode();
+    // Or another example:
+    //   int hash = name.hashCode()+email.hashCode();
         
-        return hash;
-    }
+    return hash;
+  }
     
-    @Override
-    public boolean equals(Object o) {
-        if (o==null) {
-            return false;
-        }
-
-        // please please java check if o is a Customer for us... and
-        // if not return false
-        if ( ! (o instanceof Customer) ) {
-            return false;
-        }
-
-        Customer c = (Customer) o;
-        
-        // Compare the Strings name and email , using the equals method in String
-        if (this.name.equals(c.name) && this.email.equals(c.email) ) {
-            return true;
-        }
-        
-        return false;
+  @Override
+  public boolean equals(Object o) {
+    if (o == null) {
+      return false;
     }
+
+    // please please java check if o is a Customer for us... and
+    // if not return false
+    if (! (o instanceof Customer)) {
+      return false;
+    }
+
+    Customer c = (Customer) o;
+        
+    // Compare the Strings name and email , using the equals method in String
+    if (this.name.equals(c.name) && this.email.equals(c.email)) {
+      return true;
+    }
+        
+    return false;
+  }
 
 }
