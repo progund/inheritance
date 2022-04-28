ac468c6bc306e024ddf98d0e1dc1919a2a0c94de
Rikard Fröberg <rikard@morus.se>
2022-04-28 22:39:42 +0200

    checkstyle

--- overriding_methods_in_object/com/sportmanagers/sportman/test/MemberTest.java
+++ overriding_methods_in_object/com/sportmanagers/sportman/test/MemberTest.java
@@ -1,23 +1,21 @@
 package com.sportmanagers.sportman.test;
 
-import java.util.ArrayList;
 import com.sportmanagers.sportman.Member;
+import java.util.ArrayList;
 
 public class MemberTest {
 
-    public static void main(String[] args) {
+  public static void main(String[] args) {
 
-        ArrayList<Member> register = new ArrayList<>();
+    ArrayList<Member> register = new ArrayList<>();
 
-        register.add(new Member("Ada", "ada@lovelace.org", 1));
-        register.add(new Member("Bart", "bart@simpson.net", 2));
-        register.add(new Member("Marge", "marge@simpson.net", 3));
+    register.add(new Member("Ada", "ada@lovelace.org", 1));
+    register.add(new Member("Bart", "bart@simpson.net", 2));
+    register.add(new Member("Marge", "marge@simpson.net", 3));
 
-        for (Member m : register) {
-            System.out.println(m);
-        }
-        
-    }
-
+    for (Member m : register) {
+      System.out.println(m);
+    }        
+  }
     
 }
