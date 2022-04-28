33c73471bcd99b175d8c5817475026e14e51e2e1
Rikard Fröberg <rikard@morus.se>
2022-04-28 22:37:04 +0200

    checkstyle

--- overriding_methods_in_object/com/sportmanagers/sportman/Team.java
+++ overriding_methods_in_object/com/sportmanagers/sportman/Team.java
@@ -5,50 +5,48 @@ import java.util.Arrays;
 
 public class Team {
 
-    private String    name;
+  private String name;
 
-    // List of members (later on we will change the type of this list to java.util.List)
-    private ArrayList<Member> members;
+  // List of members (later on we will change the type of this list to java.util.List)
+  private ArrayList<Member> members;
     
-    public Team(String name) {
-        this.name = name;
-        this.members = new ArrayList<>();
-    }
+  public Team(String name) {
+    this.name = name;
+    this.members = new ArrayList<>();
+  }
 
-    public Team(String name, ArrayList<Member> members) {
-        this.name    = name;
-        this.members = members;
-    }
+  public Team(String name, ArrayList<Member> members) {
+    this.name    = name;
+    this.members = members;
+  }
 
-    public String name() {
-        return name;
-    }
+  public String name() {
+    return name;
+  }
     
-    // List of members (later on we will change the type of this list to java.util.List)
-    public ArrayList<Member> members() {
-        return members;
-    }
+  // List of members (later on we will change the type of this list to java.util.List)
+  public ArrayList<Member> members() {
+    return members;
+  }
 
-    public void addMember(Member m) {
-        members.add(m);
-    }
+  public void addMember(Member m) {
+    members.add(m);
+  }
 
+  public String toString() {
+    return this.name + " members: " + members;
 
-    public String toString() {
-        return this.name + " members: " + members;
-
-        // Solution where we loop and add the String representations
-        // of our Member instances one by one
-        /*
-        result += " members: [";
-        for (Member m : members) {
-            result += m;
-            // There are better (faster) ways to append/concat many
-            // strings to a string ... Look at StringBuilder :)
-        }
-        result += " ]";
-        */
-    }
-
+    // Solution where we loop and add the String representations
+    // of our Member instances one by one
+    /*
+      result += " members: [";
+      for (Member m : members) {
+      result += m;
+      // There are better (faster) ways to append/concat many
+      // strings to a string ... Look at StringBuilder :)
+      }
+      result += " ]";
+    */
+  }
 
 }
