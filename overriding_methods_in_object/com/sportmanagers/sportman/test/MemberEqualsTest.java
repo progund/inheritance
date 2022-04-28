76d0f886c1a749b146923b533e0d86288fd85b78
Rikard Fröberg <rikard@morus.se>
2022-04-28 22:38:30 +0200

    checkstyle

--- overriding_methods_in_object/com/sportmanagers/sportman/test/MemberEqualsTest.java
+++ overriding_methods_in_object/com/sportmanagers/sportman/test/MemberEqualsTest.java
@@ -5,24 +5,21 @@ import com.sportmanagers.sportman.Member;
 
 public class MemberEqualsTest {
 
-    public static void main(String[] args) {
+  public static void main(String[] args) {
 
-        Member m1 = new Member("Ada", "ada@lovelace.org", 1);
-        Member m2 = new Member("Ada", "ada@lovelace.org", 1);
+    Member m1 = new Member("Ada", "ada@lovelace.org", 1);
+    Member m2 = new Member("Ada", "ada@lovelace.org", 1);
 
-        System.out.print("Comparing two member object with the same content. They are");
-        if ( ! m1.equals(m2)) {
-            System.out.print(" not");
-        }
-        System.out.println(" equal");
-
-        System.out.println("Hashcode for the objects:");
-        System.out.println(" * m1: " + m1.hashCode());
-        System.out.println(" * m2: " + m2.hashCode());
-
-        
-        
+    System.out.print("Comparing two member object with the same content. "
+                     + "They are");
+    if (! m1.equals(m2)) {
+      System.out.print(" not");
     }
+    System.out.println(" equal");
 
+    System.out.println("Hashcode for the objects:");
+    System.out.println(" * m1: " + m1.hashCode());
+    System.out.println(" * m2: " + m2.hashCode());        
+  }
     
 }
